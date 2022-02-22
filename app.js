25f82f76fcbfc0f93402005a2c66ec4b1609f1ef
ibrahimjal <ibrahimjal20@gmail.com>
2022-02-22 21:49:09 +0000

    Worked on the comment section

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const heartNum1 = document.getElementById('heartNum1')
 const heartNum2 = document.getElementById('heartNum2')
 const heartNum3 = document.getElementById('heartNum3')
 const heartNum4 = document.getElementById('heartNum4')
-console.log(heartNum1)
+
 
 heartNum1.textContent = "0"
 // heartNum2.textContent = "0"
@@ -46,13 +46,22 @@ document.getElementById("messageBtn").addEventListener("click", ()=> {
 // add comment 
 const msgBody = document.getElementById("msgBody")
 const msgInput1 = document.getElementById("msgInput1")
+const message = document.getElementById("message")
+const msg = document.getElementById("msg")
+console.log(msg)
+
 
 document.getElementById("addComment").addEventListener("click", ()=>{
-    console.log("Comment added")
-    paragraph = document.createElement("p")
-    paragraph.classList.add("msg-styling")
-    paragraph.textContent = msgInput1.value
-    msgBody.appendChild(paragraph)
+    // console.log("Comment added")
+    // paragraph = document.createElement("p")
+    // paragraph.classList.add("msg-styling")
+    // paragraph.textContent = msgInput1.value
+    // msgBody.appendChild(paragraph)
+    msgBody.appendChild(message)
+    message.style.display = "flex"
+    const userMessage = msgInput1.value
+    msg.textContent = userMessage
+    
 
     msgInput1.value = ""
 
